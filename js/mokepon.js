462ecb5f4c4636e720ca144b83d61daa2ce2748c
Jhosselyn30 <astridjhosselyn@gmail.com>
2024-06-11 19:35:11 -0400

    Sección mascotas

---
+++ js/mokepon.js
@@ -0,0 +1,24 @@
+
+function iniciarJuego(){
+    let  botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+}
+function seleccionarMascotaJugador(){
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya') 
+
+    if (inputHipodoge.checked){
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked){
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked){
+        alert ('Seleccionaste a Ratigueya')
+    } else {
+        alert ('No seleccionaste ninguna mascota')
+    }
+}
+
+
+
+window.addEventListener('load',iniciarJuego)  
