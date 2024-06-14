8eea44a3d9559a0ee71155a7a2b20236e392aa9a
Jhosselyn30 <astridjhosselyn@gmail.com>
2024-06-13 20:38:28 -0400

    Agregando combate, contador de vidas y resultados

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,17 @@
-let ataqueJugador //varibale global
+let ataqueJugador //variable global
 let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
+
+
 
 function iniciarJuego(){
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'none'
+
+    let sectionReiniciar = document.getElementById('boton-reiniciar')
+    sectionReiniciar.style.display='none'
+
     let  botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
 
@@ -11,10 +21,20 @@ function iniciarJuego(){
     botonAgua.addEventListener('click',ataqueAgua)
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click',ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click',reiniciarJuego)
+
     
 }
 
 function seleccionarMascotaJugador(){
+
+    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+    sectionSeleccionarMascota.style.display = 'none'
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'block'
+
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya') 
@@ -72,16 +92,79 @@ function ataqueAleatorioEnemigo(){
         ataqueEnemigo = 'TIERRA'
     }
 
-    crearMensaje()
+    combate()
+
 }
 
-function crearMensaje(){
+function combate(){
+    let spanVidasJugador = document.getElementById('vidas-jugador')
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+    
+
+    if (ataqueEnemigo == ataqueJugador){
+        crearMensaje("EMPATE")
+    } else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA'){
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else if (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO'){
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    }else if (ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA'){
+        alecrearMensajert("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else  {
+        crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+
+    revisarVidas()
+
+}
+
+function revisarVidas(){
+    if (vidasEnemigo==0){
+        crearMensajeFinal('GANASTE! 🥳')
+    } else if (vidasJugador == 0){
+        crearMensajeFinal('PERDISTE 😔')
+    }
+}
+
+function crearMensaje(resultado){//recibe como parámetro resultado
     let sectionMensajes= document.getElementById('mensajes')
 
     let parrafo = document.createElement('p')
-    parrafo.innerHTML = 'Tu mascota atacó con '+ ataqueJugador + ', la mascota del enemigo atacó con ' + ataqueEnemigo + ' - GANASTE 🥳'
+    parrafo.innerHTML = 'Tu mascota atacó con '+ ataqueJugador + ', la mascota del enemigo atacó con ' + ataqueEnemigo + ' - '+ resultado
 
     sectionMensajes.appendChild(parrafo)
+}
+
+function crearMensajeFinal(resultadoFinal){
+    let sectionMensajes= document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensajes.appendChild(parrafo)
+
+    let botonFuego =document.getElementById('boton-fuego')
+    botonFuego.disabled=true;
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled=true;
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled=true;
+
+    let sectionReiniciar = document.getElementById('boton-reiniciar')
+    sectionReiniciar.style.display='block'
+
+
+
+}
+function reiniciarJuego(){
+    location.reload()
 }
 
 function aleatorio(min,max){
